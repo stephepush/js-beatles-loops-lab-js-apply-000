e1a4c6c9a4a14998aa3150b55aff1858374ecbb2
Stephen Peters <7speter@gmail.com>
2020-02-21 05:07:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ function theBeatlesPlay(musicians, instruments){
   for (i = 0 ; i < musicians.length; i++){
     BeatlesInstruments.push(musicians[i] + " plays " + instruments[i])
   }
+  return BeatlesInstruments
 }
