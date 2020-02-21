1446c4f25096414fde42902ea8d31ce6daf0e5af
Stephen Peters <7speter@gmail.com>
2020-02-21 21:58:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ function johnLennonFacts(array){
 
 function iLoveTheBeatles(num) {
 	var arr = [];
-	var i = 1;
+	var i = 0;
 
 	do {
 		arr.push('I love the Beatles!');
 		i++;
-	} while (i < num-1);
+	} while (i < num);
 	return arr;
 }
